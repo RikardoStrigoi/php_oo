850803c66eeac471d09c8b368c659e409a182320
RikardoStrigoi <rikardostrigoi@gmail.com>
2021-05-23 22:14:37 -0300

    Primeiro Commit do curso de Avançando com Orientação a Objetos em PHP

--- banco.php
+++ banco.php
@@ -1,10 +1,13 @@
 <?php
 
 require_once 'src/Conta.php';
+require_once 'src/Endereco.php';
 require_once 'src/Titular.php';
 require_once 'src/Cpf.php';
 
-$ricardo = new Titular(new CPF('123.456.789-10'), 'Ricardo Ferreira');
+
+$endereco = new Endereco('Curitiba', 'Jardim das Americas', 'R. Prof Joao Doetzer', '873');
+$ricardo = new Titular(new CPF('123.456.789-10'), 'Ricardo Ferreira', $endereco);
 $primeiraConta = new Conta($ricardo);
 $primeiraConta->deposita(500);
 $primeiraConta->saca(300);
